245ccc901860a9c7a7b75368601f1d580bc55331
JoaoHChagas <joaohenriquedonatochagas@gmail.com>
2025-03-17 21:41:32 -0300

    Add files via upload

--- Distancia_2_pontos.java
+++ Distancia_2_pontos.java
@@ -1,17 +1,22 @@
 package com.mycompany.distancia_2_pontos;
+
+import java.util.Scanner;
+
 public class Distancia_2_pontos {
     public static void main(String[] args) {
         Scanner tc = new Scanner(System.in);
-        System.out.println("Digite o valor de x1");
+         System.out.println("Digite o valor de x1");
          double x1 = tc.nextDouble();
-        System.out.println("Digite o valor de y1");
+         System.out.println("Digite o valor de y1");
          double y1 = tc.nextDouble();
-        System.out.println("Digite o valor de x2");
+         System.out.println("Digite o valor de x2");
          double x2 = tc.nextDouble();
-        System.out.println("Digite o valor de y2");
+         System.out.println("Digite o valor de y2");
          double y2 = tc.nextDouble();
          double primeiro_termo = (x2 - x1)*(x2 - x1);
          double segundo_termo = (y2 - y1)*(y2 - y1);
          double dist = primeiro_termo + segundo_termo;
+         Math.sqrt(dist);
+         System.out.println("A distancia e: " + dist);
     }
 }
