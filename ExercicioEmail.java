20464ac138c80c4043a72653c86d6ee6d4ff2935
JoaoHChagas <joaohenriquedonatochagas@gmail.com>
2025-04-15 21:51:05 -0300

    Add files via upload

---
+++ ExercicioEmail.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicioemail;
+
+/**
+ *
+ * @author 1012510497
+ */
+public class ExercicioEmail {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
